290a153c763b720ff80d40fe97079d01f74a377c
DIKSHIT MORADIYA <dixit.moradia@phoenixcoded.co>
2025-09-08 17:03:17 +0530

    vue issue fix

--- vue/src/views/dashboards/default/components/WelcomeBanner.vue
+++ vue/src/views/dashboards/default/components/WelcomeBanner.vue
@@ -4,7 +4,7 @@ import Banner from '@/assets/images/analytics/welcome-banner.png';
 </script>
 
 <template>
-  <v-card class="welcomeBanner text-white overflow-hidden" elevation="0" rounded="lg">
+  <v-card class="welcomeBanner text-surface overflow-hidden" elevation="0" rounded="lg">
     <v-card-text class="py-5 px-md-12 px-6">
       <v-row>
         <v-col cols="12" xl="6" md="7" sm="10">
@@ -13,7 +13,15 @@ import Banner from '@/assets/images/analytics/welcome-banner.png';
             <p class="text-h6 mb-7">
               The Brand new User Interface with power of Vuetify Components. Explore the Endless possibilities with Able Pro.
             </p>
-            <v-btn color="white" :href="buyNowLink" class="text-none" target="_blank" variant="outlined" rounded="md">Download now</v-btn>
+            <v-btn
+              color="white"
+              href="https://codedthemes.com/item/able-pro-dashboard-templates/"
+              class="text-none"
+              target="_blank"
+              variant="outlined"
+              rounded="md"
+              >Download now</v-btn
+            >
           </div>
         </v-col>
         <v-col cols="12" xl="6" md="5" class="d-md-block d-none">
