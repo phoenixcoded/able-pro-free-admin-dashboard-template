21f7f79e866b4f5ecdc84bd6d3db0c943d0d25ae
ct-anjali-patel <anjali.rana@phoenixcoded.co>
2026-01-05 19:27:22 +0530

    vue free version update done,& checked all pages, eslint & build checked (#78)

--- vue/src/layouts/dashboard/vertical-sidebar/NavItem/NavItem.vue
+++ vue/src/layouts/dashboard/vertical-sidebar/NavItem/NavItem.vue
@@ -1,68 +1,61 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { computed } from 'vue';
+
 const props = defineProps({ item: Object, level: Number });
 import SvgSprite from '@/components/shared/SvgSprite.vue';
-import { string } from 'yup';
 
-const relativeURL = ref(string);
+const baseURL = import.meta.env.BASE_URL;
 
-onMounted(async () => {
-  try {
-    relativeURL.value = await import.meta.env.BASE_URL;
-  } catch (error) {
-    console.error('Error url not found:', error);
+const propsForLink = computed(() => {
+  // open in new tab with full URL
+  if (props.item.getURL) {
+    return {
+      href: baseURL + props.item.to.replace(/^\//, ''),
+      target: '_blank'
+    };
   }
+
+  // external links
+  if (props.item.type === 'external') {
+    return {
+      href: props.item.to,
+      target: '_blank'
+    };
+  }
+
+  // internal vue-router navigation
+  return {
+    to: props.item.to
+  };
 });
 </script>
 
 <template>
   <!---Single Item-->
-  <v-list-item
-    v-if="props.item.getURL === true"
-    :to="item.type === 'external' ? '' : item.to"
-    :href="`${relativeURL}${item.to}`"
-    rounded
-    color="primary"
-    :disabled="item.disabled"
-    :target="item.type === 'external' ? '_blank' : ''"
-  >
+  <v-list-item v-bind="propsForLink" rounded class="mb-1" color="primary" :disabled="props.item.disabled">
     <!---If icon-->
     <template v-slot:prepend>
       <SvgSprite :name="props.item.icon || ''" :level="props.level" />
     </template>
-    <v-list-item-title>{{ item.title }}</v-list-item-title>
+    <v-list-item-title>
+      {{ item.title }}
+      <!---If any chip or label-->
+      <v-badge :color="props.item.chipColor" v-if="props.item.chipColor === 'success'" :aria-label="props.item.chip" inline dot></v-badge>
+    </v-list-item-title>
     <!---If Caption-->
     <v-list-item-subtitle v-if="item.subCaption" class="text-caption mt-0 hide-menu">
       {{ item.subCaption }}
     </v-list-item-subtitle>
     <!---If any chip or label-->
-    <template v-slot:append v-if="item.chip">
-      <v-chip :color="item.chipColor" class="sidebarchip hide-menu" size="small" :variant="item.chipVariant" :prepend-icon="item.chipIcon">
-        {{ item.chip }}
-      </v-chip>
-    </template>
-  </v-list-item>
-  <v-list-item
-    v-else
-    :to="item.type === 'external' ? '' : item.to"
-    :href="item.type === 'external' ? item.to : ''"
-    rounded
-    color="primary"
-    :disabled="item.disabled"
-    :target="item.type === 'external' ? '_blank' : ''"
-  >
-    <!---If icon-->
-    <template v-slot:prepend>
-      <SvgSprite :name="props.item.icon || ''" :level="props.level" />
-    </template>
-    <v-list-item-title>{{ item.title }}</v-list-item-title>
-    <!---If Caption-->
-    <v-list-item-subtitle v-if="item.subCaption" class="text-caption mt-0 hide-menu">
-      {{ item.subCaption }}
-    </v-list-item-subtitle>
-    <!---If any chip or label-->
-    <template v-slot:append v-if="item.chip">
-      <v-chip :color="item.chipColor" class="sidebarchip hide-menu" size="small" :variant="item.chipVariant" :prepend-icon="item.chipIcon">
+    <template v-if="props.item.chip && props.item.chipColor !== 'success'" #append>
+      <v-chip
+        :color="item.chipColor"
+        :border="item.chipVariant === 'tonal' ? `${item.chipColor} solid thin opacity-50` : ''"
+        class="sidebarchip hide-menu"
+        size="x-small"
+        :variant="item.chipVariant"
+        :prepend-icon="item.chipIcon"
+      >
         {{ item.chip }}
       </v-chip>
     </template>
