d360b57f3ebcacaece4a1871722fd194deaea32e
ct-nensy <nensy.savsaviya@codedthemes.com>
2026-06-15 16:23:56 +0530

    Angular22 package update & code improvement

--- angular/eslint.config.mjs
+++ angular/eslint.config.mjs
@@ -1,49 +1,47 @@
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [{
-    ignores: ["projects/**/*"],
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@angular-eslint/recommended",
-    "plugin:@angular-eslint/template/process-inline-templates",
-).map(config => ({
-    ...config,
-    files: ["**/*.ts"],
-})), {
-    files: ["**/*.ts"],
-
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+ 
+export default tseslint.config(
+  {
+    ignores: ['projects/**/*']
+  },
+ 
+  {
+    files: ['**/*.ts'],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...angular.configs.tsRecommended
+    ],
+    processor: angular.processInlineTemplates,
+ 
     rules: {
-        "@angular-eslint/directive-selector": ["error", {
-            type: "attribute",
-            prefix: "app",
-            style: "camelCase",
-        }],
-
-        "@angular-eslint/component-selector": ["error", {
-            type: "element",
-            prefix: "app",
-            style: "kebab-case",
-        }],
-    },
-}, ...compat.extends(
-    "plugin:@angular-eslint/template/recommended",
-    "plugin:@angular-eslint/template/accessibility",
-).map(config => ({
-    ...config,
-    files: ["**/*.html"],
-})), {
-    files: ["**/*.html"],
-    rules: {},
-}];
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase'
+        }
+      ],
+ 
+      '@angular-eslint/component-class-suffix': 'off',
+ 
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case'
+        }
+      ]
+    }
+  },
+ 
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended],
+    rules: {}
+  }
+);
